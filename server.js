7eb63aebd6f3c67970e53a598b501cd0fdbd4594
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-05-03 19:34:33 -0500

    Add new CRUD endpoints for new table

--- server.js
+++ server.js
@@ -56,6 +56,51 @@ app.delete('/explorers/:id', async (req, res) => {
 	return res.json({message: "Eliminado correctamente"});
 });
 
+app.get("/explorers2", async (req, res) => {
+    const allExplorers = await prisma.explorer2.findMany({});
+    res.json(allExplorers);
+  });
+  
+app.get("/explorers2/:id", async (req, res) => {
+    const id = req.params.id;
+    const explorer = await prisma.explorer2.findUnique({
+      where: { id: parseInt(id) },
+    });
+    res.json(explorer);
+  });
+  
+  app.post("/explorers2", async (req, res) => {
+    const explorer = {
+      name: req.body.name,
+      username: req.body.username,
+      mission: req.body.mission,
+    };
+    const message = "Explorer creado.";
+    await prisma.explorer2.create({ data: explorer });
+    return res.json({ message });
+  });
+  
+  app.put("/explorers2/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    await prisma.explorer2.update({
+      where: {
+        id: id,
+      },
+      data: {
+        mission: req.body.mission,
+      },
+    });
+  
+    return res.json({ message: "Actualizado correctamente" });
+  });
+  
+  app.delete('/explorers2/:id', async (req, res) => {
+      const id = parseInt(req.params.id);
+      await prisma.explorer2.delete({where: {id: id}});
+      return res.json({message: "Eliminado correctamente"});
+  });
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
